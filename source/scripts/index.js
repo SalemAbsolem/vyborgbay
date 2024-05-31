21bebffee643cbf90867750f7069b08fe48f2130
SalemAbsolem <smirnovalexander.cat@gmail.com>
2024-05-31 14:57:45 +0300

    feat: стилизация для 320px + подключение графики

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,24 @@
-/* в этот файл добавляет скрипты*/
+// var BookingIframe = new BookingIframe({
+//   html_id: 'booking_iframe',
+//   uid: 'c5c1ddb7-46a0-449a-984a-928f829675e5',
+//   lang: 'ru',
+//   width: 'auto',
+//   height: 'auto',
+//   rooms: '',
+//   scroll_to_rooms: '1'
+// });
+// BookingIframe.init();
+
+// const moduleBooking = document.querySelector('#booking_iframe iframe');
+// moduleBooking.addEventListener('load', function sizeIframe() {
+//   const repeatInterval = setInterval(() => {
+//     if(moduleBooking.style.height !== '300px') {
+//       moduleBooking.style.minHeight = '300px';
+//       moduleBooking.style.height = '300px';
+//     } else {
+//       clearInterval(repeatInterval);
+//       moduleBooking.removeEventListener('load', sizeIframe);
+//     }
+//   }, 100);
+
+// });
