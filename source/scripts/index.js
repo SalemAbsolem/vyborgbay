074d1c91134eb20bdd3a29f5c4f5da322ff3ab52
SalemAbsolem <smirnovalexander.cat@gmail.com>
2024-05-31 20:10:07 +0300

    feat: добавляет состояния кнопкам

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -22,3 +22,23 @@
 //   }, 100);
 
 // });
+
+const header = document.querySelector('.header');
+const menuButton = header.querySelector('.button--menu');
+const menuButtonText = menuButton.querySelector('.button__text');
+const menu = header.querySelector('.site-navigation.site-navigation--header');
+
+menuButton.addEventListener('click', () => {
+  header.classList.toggle('header--nav-is-open');
+  menu.classList.toggle('site-navigation--is-open');
+
+  if(menu.classList.contains('site-navigation--is-open')) {
+    menuButtonText.textContent = 'Закрыть';
+    menuButton.classList.add('button--brown-white');
+    menuButton.classList.remove('button--white-brown');
+  } else {
+    menuButtonText.textContent = 'Меню';
+    menuButton.classList.remove('button--brown-white');
+    menuButton.classList.add('button--white-brown');
+  }
+});
