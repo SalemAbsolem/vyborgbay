d934bbb87707c1c164231d7eb31013d9f30f5b50
SalemAbsolem <smirnovalexander.cat@gmail.com>
2024-06-01 14:34:46 +0300

    feat: завершает стилизацию для 320px. Подключает масштабирование

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -23,10 +23,26 @@
 
 // });
 
+// Брейкпоинты
+const mobileOnlyBP = window.matchMedia('screen and (max-width: 767.98px)');
+const notLargeDesctopBP = window.matchMedia('screen and (max-width: 1919.98px)');
+const tabletOnlyBP = window.matchMedia('screen and (min-width: 768px) and (max-width: 1199.98px)');
+const smallDesctopOnlyBP = window.matchMedia('screen and (min-width: 1200px) and (max-width: 1919.98px)');
+const feedbackBlockBP = window.matchMedia('screen and (max-width: 480.98px)');
+
+// Открытие меню
+
 const header = document.querySelector('.header');
 const menuButton = header.querySelector('.button--menu');
 const menuButtonText = menuButton.querySelector('.button__text');
 const menu = header.querySelector('.site-navigation.site-navigation--header');
+const pageList = menu.querySelectorAll('.pages-list__item').length;
+
+let menuHeight;
+
+if(mobileOnlyBP.matches) {
+  menuHeight = 150 + (39 * pageList) - 10;
+}
 
 menuButton.addEventListener('click', () => {
   header.classList.toggle('header--nav-is-open');
@@ -36,9 +52,105 @@ menuButton.addEventListener('click', () => {
     menuButtonText.textContent = 'Закрыть';
     menuButton.classList.add('button--brown-white');
     menuButton.classList.remove('button--white-brown');
+    menu.style.height = `${menuHeight}px`;
   } else {
     menuButtonText.textContent = 'Меню';
     menuButton.classList.remove('button--brown-white');
     menuButton.classList.add('button--white-brown');
+    menu.style.height = '';
   }
 });
+
+// Аккордион в футере
+
+const footer = document.querySelector('.footer');
+
+if(mobileOnlyBP.matches) {
+  const footerGroups = footer.querySelectorAll('.footer__column--accordion');
+  const footerTitles = [];
+  const groupsHeight = [];
+  const titleHeight = [];
+
+  for(let i = 0; i < footerGroups.length; i++) {
+    footerTitles[i] = footerGroups[i].querySelector('.footer__column-title');
+    titleHeight[i] = footerTitles[i].clientHeight;
+    groupsHeight[i] = footerGroups[i].clientHeight;
+
+    for(let j = 0; j < footerGroups.length; j++) {
+      footerGroups[j].style.height = `${titleHeight[j]}px`;
+    }
+
+    footerGroups[0].style.height = `${groupsHeight[0]}px`;
+
+    footerTitles[i].addEventListener('click', () => {
+      for(let j = 0; j < footerGroups.length; j++) {
+        footerGroups[j].classList.remove('footer__column--active');
+        footerGroups[j].style.height = `${titleHeight[j]}px`;
+      }
+      footerGroups[i].classList.add('footer__column--active');
+      footerGroups[i].style.height = `${groupsHeight[i]}px`;
+    });
+  }
+}
+
+//  Масштабирование
+
+let widthScreen = window.innerWidth;
+
+let scale;
+if(mobileOnlyBP.matches) {
+  scale = widthScreen / 320;
+} else if(tabletOnlyBP.matches) {
+  scale = widthScreen / 768;
+} else if(smallDesctopOnlyBP.matches) {
+  scale = widthScreen / 1200;
+}
+
+if(notLargeDesctopBP.matches) {
+  const sectionOnMain = document.querySelectorAll('.main__section:not(.feedback)');
+  const sectionFeedback = document.querySelector('.main__section.feedback');
+  const sectionFeedbackTitle = sectionFeedback.querySelector('.main__section-title');
+  const sectionFeedbackWidjet = sectionFeedback.querySelector('.sw-app');
+
+  if(scale >= 1) {
+    header.setAttribute('style', `zoom: ${scale}`);
+
+    for(let i = 0; i < sectionOnMain.length; i++) {
+      sectionOnMain[i].setAttribute('style', `zoom: ${scale}`);
+    }
+
+    sectionFeedbackTitle.setAttribute('style', `zoom: ${scale}`);
+    if(feedbackBlockBP.matches) {
+      sectionFeedbackWidjet.setAttribute('style', `zoom: ${scale}`);
+    }
+
+    footer.setAttribute('style', `zoom: ${scale}`);
+  }
+
+  window.addEventListener('resize', () => {
+    widthScreen = window.innerWidth;
+
+    if(mobileOnlyBP.matches) {
+      scale = widthScreen / 320;
+    } else if(tabletOnlyBP.matches) {
+      scale = widthScreen / 768;
+    } else if(smallDesctopOnlyBP.matches) {
+      scale = widthScreen / 1200;
+    }
+
+    if(scale >= 1) {
+      header.setAttribute('style', `zoom: ${scale}`);
+
+      for(let i = 0; i < sectionOnMain.length; i++) {
+        sectionOnMain[i].setAttribute('style', `zoom: ${scale}`);
+      }
+
+      sectionFeedbackTitle.setAttribute('style', `zoom: ${scale}`);
+      if(feedbackBlockBP.matches) {
+        sectionFeedbackWidjet.setAttribute('style', `zoom: ${scale}`);
+      }
+
+      footer.setAttribute('style', `zoom: ${scale}`);
+    }
+  });
+}
